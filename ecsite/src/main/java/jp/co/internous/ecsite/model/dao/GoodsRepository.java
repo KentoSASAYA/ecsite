e94138187cae8bb0759032e819d26447a4930c71
KentoSASAYA <kentosasaya1994@gmail.com>
2022-05-27 16:38:39 +0900

    ECサイト

---
+++ ecsite/src/main/java/jp/co/internous/ecsite/model/dao/GoodsRepository.java
@@ -0,0 +1,13 @@
+package jp.co.internous.ecsite.model.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.co.internous.ecsite.model.entity.Goods;
+
+public interface GoodsRepository extends JpaRepository<Goods, Long> {
+
+	List<Goods> findAll();
+
+}
